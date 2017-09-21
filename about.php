9a854f34286f0de8bb9db4dfa5c27ff40c30a816
bsawyers22 <>
2017-09-21 16:28:32 -0400

    initialization, added main and about pages and intial code

---
+++ about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>     
+        <title> About the Project </title> 
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <meta name="description" content="brief description of the page"> 
+        <link rel="shortcut icon" href="images/favicon.jpg"> 
+        <link rel="stylesheet" href="css/normalize.css"> 
+        <link rel="stylesheet" href="css/main.css">  
+    </head>  
+    <body> 
+        <div id="page2">
+            <?php
+            echo "<header><a href='index.php'><h1>Python 3</a></h1></header>";
+            ?>
+
+
+            <main>
+                <h2>About the Project</h2>
+                <section>
+                    <ul>
+                        <li>Javascript: Completed by Ben</li>
+                        <li>HTML: Completed by Gage</li>
+                        <li>Information:
+                            <ul>
+                                <li>Questions 1-3: Completed by Gage</li>
+                                <li>Questions 4-5: Completed by Ben</li>
+                            </ul>
+                        </li>
+                        <li>Accordion: Completed by Ben</li>
+                    </ul>
+                </section>
+
+            </main>
+        </div>
+
+
+        <?php
+        echo "<footer> Benjamin Sawyers & Kayvan Ehteshami - CS3800 Project 1 - Fall 2017 </footer>";
+        ?>
+    </body>
+
+</html> 
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -6,12 +6,66 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title> Project 1 </title> 
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <meta name="description" content="project 1 cs3800"> 
+        <link rel="shortcut icon" href="images/favicon.jpg"> 
+        <link rel="stylesheet" href="css/normalize.css"> 
+        <link rel="stylesheet" href="css/main.css">  
     </head>
     <body>
         <?php
-        // put your code here
+        echo "<header><h1>Revolver Ocelot</h1> </header>";
+        echo "<nav><ul><a href='about.php'><li>About</a></li></ul></nav>";
         ?>
+        <main>
+            <div id="accordion">
+                <h2>Creation</h2>
+                <div>
+                    <p>
+                        Python was created by Guido van Rossum during the 1980’s.  For Rossum creating Python started off as a hobby.  Python is now growing in popularity due to its reliability, ease of use, and expansive collections of libraries.  Python was not given its name based off of the reptile.  Rossum based the name of Monty Python’s Flying Circus.
+                    </p>
+                </div>
+                <h2>Interaction</h2>
+                <div>
+                    <p>
+                        While Python 3 is not usually directly used to interact with the client side there are many interpreters that will convert Python 3 into useable JavaScript that can easily be used on the user's machine.  This allows for a Python 3 to be both a server sided tool, and a client sided tool.  One such interpreter is pyjs.
+                    </p>
+                </div>
+                <h2>Libraries</h2>
+                <div>
+                    <p>
+                        Python 3 like many other popular programming languages supports many useful libraries.  Some of these include HTML, XML, and JSON.  Python 3 also supports internet protocols with a well rounded socket interface.  Some of the protocols it supports are FTP, and IMAP.
+                    </p>
+                </div>
+                <h2>Usage</h2>
+                <div>
+                    <p>
+                        Since the turn of the century Python has been one of the top 10 programming languages according to TIOBE Programming Community Index.  This is partly due to its ease of use, and reliability.  Pythons primary usage is to be used as a scripting language or for web applications.  With Pythons large collection of libraries it can be used for many other things.  Such as client side development, 2d/3d visualization, macro scripting, and much more.
+                    </p>
+                </div>
+                <h2>Orientation</h2>
+                <div>
+                    <p>
+                        Python like many other popular languages used today is an object oriented programming language.  Some other langauges that follow object oriented programming styles are Java, C++, and C#.  
+                    </p>
+                </div>
+            </div>
+
+
+            <a href="question1.html"><input type="button" id="begin" value="Begin Quiz"/></a>
+
+
+        </main>
+
+        <?php
+        echo "<footer> Benjamin Sawyers & Kayvan Ehteshami - CS3800 Project 1 - Fall 2017 </footer>";
+        ?>
+
     </body>
 </html>
+
+
+
+
