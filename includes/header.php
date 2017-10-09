10caf09ce645a491bc1b237a1bb66620d8aa16a1
PROGENITOR_01B\kevin <reversedfool@hotmail.com>
2017-10-09 18:20:21 -0400

    functional banner

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 
 <?php
 
-echo "<header><h1>Revolver Ocelot</h1> </header>";
+echo "<header><video width='120' height='100' id='fidgetspinner1' autoplay loop muted='true'><source src='images/ocelotvid.mp4' type='video/mp4'></video> <h1>Revolver Ocelot</h1> <video width='120' height='100' id='fidgetspinner2' autoplay loop muted='true'><source src='images/ocelotvid.mp4' type='video/mp4'></video></header>";
 echo "<nav><ul><a href='index.php'><li>Home</a></li><a href='about.php'><li>About</a></li><a href='history.php'><li>History</a></li><li><a href='quizquestions.php'>Quiz</a></li></ul></nav>";
 ?>
 
